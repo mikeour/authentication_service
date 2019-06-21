76504b96c6425283186db726f397de39300a8c22
Michael Roeslein <michaeljroeslein@gmail.com>
2019-06-21 16:19:01 -0400

    Added body-parser middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const env = require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const apiRouter = require("./routes/apiRouter.js");
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.json());
 app.use(express.static("client/dist"));
 app.use("/api", apiRouter);
 
