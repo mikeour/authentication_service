0e59b54d1976ec44cfc2b4b7d5d235f0c9634adc
Michael Roeslein <michaeljroeslein@gmail.com>
2019-06-21 12:24:28 -0400

    Created AuthContext

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "../context/AuthContext";
 import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
@@ -7,29 +8,31 @@ import Secret from "./Secret";
 
 const App = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/secret">Secret</Link>
-        </li>
-      </ul>
+    <AuthProvider>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/secret">Secret</Link>
+          </li>
+        </ul>
 
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path="/secret" component={Secret} />
-      </Switch>
-    </div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/secret" component={Secret} />
+        </Switch>
+      </div>
+    </AuthProvider>
   );
 };
 
